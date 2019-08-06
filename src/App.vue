86a573f4b88701bdb2743f61786272a4304f361e
Alaila Ladera <aladera@typeiqs.com>
2019-08-06 19:23:55 -0400

    navbar component was created

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
     <div id="app" class="bg-gray-100 h-screen">
+        <Nav/>
         <TodoList/>
     </div>
 </template>
 
 <script>
 import TodoList from './components/TodoList.vue'
+import Nav from './components/Nav.vue'
 
 export default {
     name: 'app',
     components: {
-        TodoList
+        TodoList,
+        Nav
     }
 }
 </script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
-        <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App</h1>
-
+        
         <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg">
             Todo List
         </todo-card>
