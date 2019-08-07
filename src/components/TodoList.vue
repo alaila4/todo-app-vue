4cc5527cd14c0201d0be3f663c17cc94ee8429bc
Alaila Ladera <aladera@typeiqs.com>
2019-08-07 10:28:23 -0400

    space

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container mx-auto px-4 py-4 flex flex-col flex-wrap">
         <h1 class="text-xl text-center">Welcome to Your <strong>Todo App</strong></h1>
+        
         <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg">
             Todo List
         </todo-card>
