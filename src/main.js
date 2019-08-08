270c7e04bef731bcd5c2c2bcb32cac394a1d05bb
Alaila Ladera <aladera@typeiqs.com>
2019-08-08 16:31:16 -0400

    component input and button were created

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import '@/assets/css/tailwind.css';
 import Card from './components/Card.vue';
 import Task from './components/Task.vue';
 import TasksList from './components/TasksList.vue';
+import Input from './components/Input.vue';
+import Button from './components/Button.vue';
 
 
 Vue.config.productionTip = false;
@@ -12,6 +14,8 @@ Vue.config.productionTip = false;
 Vue.component('todo-card', Card);
 Vue.component('todo-task', Task);
 Vue.component('todo-tasks-list', TasksList);
+Vue.component('todo-input', Input);
+Vue.component('todo-button', Button);
 
 
 new Vue({
