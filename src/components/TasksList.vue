90b36d5e46fcba8871c2602cc2e714bd3e22886f
Ronald <rpereira@typeiqs.com>
2019-08-08 23:50:57 -0400

    Added edit tasks
    switch between input and h1

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,22 +1,25 @@
 <template>
   <div>
-    <div
-      v-for="(task, index) in tasks"
-      :key="task.id"
-      class="container flex bg-gray-100 shadow p-4 my-1 rounded"
-    >
-      <div class="w-11/12 mr-20">
-        <h1 class="text-grey-400">{{task.title}}</h1>
+    <div v-for="(task, index) in tasks"
+    :key="task.id" class="container flex bg-gray-100 shadow p-4 my-1 rounded">
+      <div class="w-11/12 mr-20 cursor-pointer" v-if="!editing">
+        <h1 class="text-grey-400" @click="enableEditing">{{task.title}}</h1>   
       </div>
-      <div class="cursor-pointer" @click="completeTask(index)">
-        <i
-          class="font-bold text-blue-700 rounded-full h-6 w-6 bg-blue-200 flex py-1 items-center justify-center"
-        >&check;</i>
+      <div v-if="editing" class="flex-1">
+        <input class="bg-gray-300 
+        focus:outline-none 
+        focus:shadow-outline 
+        border border-gray-300 
+        rounded-lg block w-full 
+        appearance-none leading-normal"
+        v-model="task.title"
+        v-on:keyup.enter="editTask">
       </div>
-      <div class="w-1/12 ml-20 cursor-pointer" @click="removeTask(index)">
-        <i
-          class="font-bold text-red-700 rounded-full h-5 w-5 bg-red-200 flex py-1 items-center justify-center"
-        >&Chi;</i>
+      <div class="px-8" @click="completeTask(index)">
+        <i class="font-bold text-blue-700 rounded-full h-6 w-6 bg-blue-200 flex items-center justify-center cursor-pointer">&check;</i>
+      </div>
+      <div @click="removeTask(index)">
+        <i class="font-bold text-red-700 rounded-full h-6 w-6 bg-red-200 flex items-center justify-center cursor-pointer">&Chi;</i>
       </div>
     </div>
   </div>
@@ -27,14 +30,14 @@ export default {
     name: "todo-task-list",
     props: {
         tasks: {
-        type: Array,
-        required: false
+          type: Array,
+          required: false
         }
     },
 
     data() {
         return {
-
+          editing: false,
         };
     },
 
@@ -46,6 +49,12 @@ export default {
         completeTask(index) {
           this.$emit("add-to-done-list", this.tasks[index]);
           this.tasks.splice(index, 1);
+        },
+        editTask(){
+          this.editing = false
+        },
+        enableEditing(){
+          this.editing = true
         }
     },
     
