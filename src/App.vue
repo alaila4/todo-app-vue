6d2b8b41286cd5d25c798d48a9421f6732513e76
Alaila Ladera <aladera@typeiqs.com>
2019-08-09 11:48:53 -0400

    delete space

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
     <div id="app" class="bg-gray-100 h-screen">
         <!-- <Nav/> -->
-        
         <div class="container mx-auto flex justify-center h-48 mx-2 px-2 my-2 py-2">
             <img class="max-w-xs object-contain object-center flex-1" src="./assets/img/type_logo.png">
         </div>        
